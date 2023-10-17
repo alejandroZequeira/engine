54d8a8a19c33ae6938da6180920592411097f7e1
Alejandro de la Rosa Zequeira <65790899+alejandroZequeira@users.noreply.github.com>
2023-10-17 15:34:34 -0600

    update simulator

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import threading
 from temp import Timer
 import time 
+global t
 t =Timer
 
 global stop
@@ -9,71 +10,89 @@ global statsTime,statsPkt
 statsTime=[]
 statsPkt=[]
 stop=True
-
-def pktGenerator():
-    pktSize=random.randint(1,20)
+def dado(max,min):
+    return random.randint(max,min)
+def pktGenerator(max , min):
+    pktSize=random.randint(min,max)
     return pktSize
 
 def on(tOn,t):
-    tActual= t.timeNow(t)
+    tActual= t.timeNow()
     print("ento en on")
     while tActual<tOn:
         print("pedir paquete")
-        tActual= t.timeNow(t)
+        tActual= t.timeNow()
         print("tiempo actual",tActual)
 
+def set_stats_time(etq,t_incial,t_final):
+    global statsTime
+    statsTime.append({
+            "tiempo_inicial": t_incial,
+            "tiempo_final":t_final,
+            "etiqueta":etq
+        })
+
 def simulacion():
-    global stop
+    global stop,t
     conton=0 
-    tSimulacion=30
+    tSimulacion=10
     contoff=0
-    ton=random.randint(1,3)
-    toff=random.randint(1,3)
-    state=random.randint(0,2)
+    ton=dado(1,3)*1000
+    toff=dado(1,3)
+    state=dado(0,2)
     if(state==1):
-        on(ton+t.timeNow(t),t)
+        on(ton+t.timeNow(),t)
+        set_stats_time("on",t.timeNow()/1000,(t.timeNow()/1000)+ton/1000)
         conton+=1
     else:
         print("entro en off")
         contoff+=1
         time.sleep(toff)
-    tOn=ton+t.timeNow(t)
-    tOff=toff+t.timeNow(t)   
+        set_stats_time("on",t.timeNow()/1000,(t.timeNow()/1000)+toff)
+    tOn=ton+(t.timeNow()/1000)
+    tOff=toff+t.timeNow()/1000   
     while stop:
         if(state==1):
             if tOff<tSimulacion:
                 time.sleep(toff)
                 print("duro en off: ",toff)
                 contoff+=1
+                set_stats_time("off",tOn,tOff)
             if tOn<tSimulacion:
-                on(ton+t.timeNow(t),t)
+                on(ton+t.timeNow(),t)
                 print("duro en on: ",ton)
+                set_stats_time("on",tOff,tOn)
                 conton+=1
         else:
             if tOff<tSimulacion:
                 time.sleep(toff)
                 print("duro en off: ",toff)
+                set_stats_time("off",tOn,tOff)
                 contoff+=1
             if tOn<tSimulacion:
-                on(ton+t.timeNow(t),t)
+                on(ton+t.timeNow(),t)
+                set_stats_time("on",tOff,tOn)
                 print("duro en on: ",ton)
                 conton+=1
-        ton=random.randint(1,3)
-        toff=random.randint(1,3)
-        tOn=ton+t.timeNow(t)
+        ton=dado(1,3)*1000
+        toff=dado(1,3)
+        tOn=(ton+t.timeNow())/1000
         tOff=toff+tOn
-        print("simulando")
+       # print("simulando")
     print("entro a on: ",conton)
     print("entro a off: ",contoff)
         
 def service():
     global stop
+    cont=0
     while(stop):
-        print("procesando paquete")
+        #print("procesando paquete")
+        cont+=1
 
 if __name__== '__main__':
     thread = threading.Thread(target=simulacion)
     thread.start()
-    thread2= threading.Thread(target=service)
-    thread2.start()
-    stop=t.istime(t ,30)
+    #thread2= threading.Thread(target=service)
+    #thread2.start()
+    stop=t.istime(10)
+    print(statsTime)
--- temp.py
+++ temp.py
@@ -1,16 +1,20 @@
-import time
+import time 
 
 class Timer:
+    global t
     t=0
     def __init__(self):
         self.t=0
     
-    def timeNow(self):
-        return self.t    
+    def timeNow():
+        global t
+        return t    
     
-    def istime(self, t):
-        while(t>0):
-            time.sleep(1)
-            t-=1
-            self.t+=1
+    def istime(ts):
+        global t
+        tsim=ts*1000
+        for i in range(1,tsim+1):
+            time.sleep(1/1000)
+            #print(i)
+            t=i
         return False
